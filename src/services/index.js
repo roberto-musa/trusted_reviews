c59346db56ae71a11b734dbdb3e1807762822602
Roberto Musa <info@robertomusa.it>
2025-08-04 17:39:09 +0200

    fix service users

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,9 @@
 import { users } from './users/users.js'
 import { reviewsBackend } from './reviews-backend/reviews-backend.js'
-import { user } from './users/users.js'
+// src/services/index.js - MODO ERRATO
+// import { user } from './users/users.js'
+// src/services/index.js - MODO CORRETTO
+import { users } from './users/users.js'
 export const services = app => {
   app.configure(users)
 
