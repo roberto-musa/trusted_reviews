6ef4dba238587661932148a4a88c732ed76dd117
Roberto Musa <info@robertomusa.it>
2025-08-04 18:00:32 +0200

    fix envVars

--- knexfile.js
+++ knexfile.js
@@ -3,21 +3,6 @@
 // Carica la configurazione delle variabili d'ambiente (importante all'inizio)
 require('dotenv').config();
 
-// Importa l'applicazione Feathers
-const { app } = require('./src/app');
-
-// Ottieni la configurazione del database DOPO che l'app è stata caricata
-// Il metodo app.get() restituisce la configurazione "risolta"
-const dbConfig = app.get('mysql');
-
-// Esporta la configurazione per i vari ambienti di Knex
-module.exports = {
-  development: dbConfig,
-  test: dbConfig,
-  production: dbConfig
-};
-
-/* OLD FILE
 // For more information about this file see https://dove.feathersjs.com/guides/cli/databases.html
 import { app } from './src/app.js'
 
@@ -25,4 +10,3 @@ import { app } from './src/app.js'
 const config = app.get('mysql')
 
 export default config
-*/
